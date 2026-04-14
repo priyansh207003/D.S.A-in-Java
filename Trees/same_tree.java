97b75c0cd4a7abbde4582f0e1ec5ac22dac40112
Priyansh Tiwari <ec22_priyanshtiwari@ggct.co.in>
2026-04-14 22:38:04 +0530

    Create same_tree.java

---
+++ Trees/same_tree.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p==null && q==null){
+            return true;
+        }
+        if(p==null || q==null){
+            return false;
+        }
+        if(p.val!=q.val){
+            return false;
+        }
+        boolean f1 = isSameTree( p.left, q.left); 
+         boolean f2 = isSameTree( p.right, q.right); 
+         if(f1==true && f2==true){
+            return true;
+         }
+         return false;
+    }
+}
