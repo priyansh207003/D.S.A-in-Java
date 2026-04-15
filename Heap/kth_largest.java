3bb3b8fffeaf573478d8fa4080ee7884ba4a70a6
Priyansh Tiwari <ec22_priyanshtiwari@ggct.co.in>
2026-04-15 17:10:10 +0530

    Create kth_largest.java

---
+++ Heap/kth_largest.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer>pq = new PriorityQueue<>(k);
+        for(int val:nums){
+            pq.add(val);
+            if(pq.size()>k){
+                pq.remove();
+            }
+        }
+        return pq.peek();
+    }
+}
